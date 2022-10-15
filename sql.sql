1b70046b14963b45f80966d5b6281cf72f102e53
vasev85 <vasev85@gmail.com>
2022-10-15 19:47:56 +0300

    Create sql.sql

---
+++ sql.sql
@@ -0,0 +1,98 @@
+show grants for sys_temp@localhost;
+select user,host,plugin from mysql.user;
+REVOKE ALL PRIVILEGES ON *.* FROM `sys_temp`@`localhost`;
+GRANT SELECT, USAGE  ON sakila.* TO `sys_temp`@`localhost`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON mysql.* TO `sys_temp`@`localhost`;
+REVOKE SELECT  ON *.*  FROM  `sys_temp`@`localhost`;
+
+select DISTINCT a.district, a.address  ,UPPER (LEFT (a.district ,1)) as pr,
+(right (a.district ,1)) as pr2 
+from address a 
+WHERE UPPER (LEFT (a.district ,1)) = 'K' and LOWER(right (a.district ,1)) = 'a' and a.district NOT  LIKE '% %'
+order by district;
+
+SELECT  DISTINCT a.district, SUBSTRING_INDEX(SUBSTRING_INDEX(a.address, ' ', -2) , ' ' ,1) as pro,  
+UPPER( LEFT (SUBSTRING_INDEX(SUBSTRING_INDEX(a.address, ' ', -2) , ' ' ,1)  ,1)) as pr1,
+LOWER( right (SUBSTRING_INDEX(SUBSTRING_INDEX(a.address, ' ', -2) , ' ' ,1)  ,1)) as pr2
+from address a 
+WHERE UPPER( LEFT (SUBSTRING_INDEX(SUBSTRING_INDEX(a.address, ' ', -2) , ' ' ,1)  ,1)) = 'K'  AND 
+      LOWER( right (SUBSTRING_INDEX(SUBSTRING_INDEX(a.address, ' ', -2) , ' ' ,1)  ,1)) = 'a' AND
+      LENGTH (SUBSTRING_INDEX(SUBSTRING_INDEX(a.address, ' ', -2) , ' ' ,1)) = 
+      LENGTH (replace( SUBSTRING_INDEX(SUBSTRING_INDEX(a.address, ' ', -2) , ' ' ,1), ' ', ''))
+order by district; 
+
+
+SELECT PARSENAME(REPLACE(a.address, ' ', '.'), 2) 
+from address a ;
+
+SELECT  (SUBSTRING_INDEX(a.address, ' ',-2) ) as pos, Left (pos,1)
+from address a ;
+
+SELECT  DISTINCT a.district, a.address  ,UPPER (LEFT (a.district ,1)) as pr,
+(right (a.district ,1)) as pr2 
+from address a 
+WHERE UPPER (LEFT (a.district ,1)) = 'K' and LOWER(right (a.district ,1)) = 'a' and length(a.district) = length(replace(a.district , ' ', ''))
+order by district;
+
+SELECT p.amount , p.payment_date 
+FROM payment p 
+WHERE p.payment_date BETWEEN '2005-06-15' AND '2005-06-18'
+      AND p.amount  >10
+ORDER BY   p.amount DESC;
+
+
+
+SELECT  f.title, p.amount, p.payment_date p_date1
+FROM payment p 
+JOIN rental r  ON r.rental_id  = p.rental_id 
+JOIN inventory i   ON r.inventory_id  = i.inventory_id  
+JOIN film f  ON f.film_id  = i.film_id  
+ORDER BY    p_date1 DESC 
+LIMIT 5;
+
+SELECT REPLACE (LOWER( c.first_name),'ll','pp') nam,  REPLACE(LOWER ( c.last_name), 'll', 'pp') , c.active 
+FROM customer c 
+WHERE (c.first_name = 'Kelly' or c.first_name = 'Willie') AND c.active = 1 
+ORDER BY nam
+;  
+
+
+SELECT c.email,SUBSTRING_INDEX(c.email, '@', 1),
+CONCAT(  LEFT (UPPER (SUBSTRING_INDEX(c.email, '@', 1)),1) , LOWER( SUBSTR((SUBSTRING_INDEX(c.email, '@', 1)),2)) ) as first_e,
+SUBSTRING_INDEX(c.email , '@', -1),
+CONCAT(  LEFT (UPPER (SUBSTRING_INDEX(c.email, '@', -1)),1) , LOWER( SUBSTR((SUBSTRING_INDEX(c.email, '@', -1)),2))) as second_e
+FROM customer c 
+;
+
+
+
+SELECT  CONCAT(s.first_name, ' ' ,s.last_name) AS  ФИО, c.city, COUNT( c2.customer_id)  AS Число_Покупателей  
+FROM staff s 
+JOIN address a ON s.address_id  = a.address_id 
+JOIN city c ON a.city_id = c.city_id 
+JOIN store s2  ON s2.store_id = s.store_id 
+JOIN customer c2 ON s2.store_id = c2.store_id  
+GROUP BY s.first_name, s.last_name, c.city 
+HAVING Число_Покупателей > 300
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
